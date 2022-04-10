bebe785b2e4959dcb5f3fef8a6074e883b318dd6
JYB <alex.ybjang@gmail.com>
2022-04-10 22:24:12 +0900

    Initial commit

---
+++ src/main/java/com/study/springboardpractice1/SpringBoardPractice1Application.java
@@ -0,0 +1,13 @@
+package com.study.springboardpractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardPractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardPractice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/springboardpractice1/SpringBoardPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboardpractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
