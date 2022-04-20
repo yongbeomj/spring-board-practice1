73d64461cead097bdebfa51310b2335f6a22b03e
JYB <alex.ybjang@gmail.com>
2022-04-20 21:39:43 +0900

    bootstrap 적용, header/controller 수정

--- src/main/java/com/study/springboardpractice1/controller/BoardController.java
+++ src/main/java/com/study/springboardpractice1/controller/BoardController.java
@@ -1,12 +1,28 @@
 package com.study.springboardpractice1.controller;
 
+import com.study.springboardpractice1.domain.dto.BoardDto;
+import com.study.springboardpractice1.domain.dto.MemberDto;
+import com.study.springboardpractice1.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    BoardService boardService;
+
+    @Autowired
+    HttpServletRequest request;
+
 
     // 게시물 목록
     @GetMapping("/board/boardlist")
@@ -18,6 +34,19 @@ public class BoardController {
     @GetMapping("/board/boardwrite")
     public String boardwrite(){
         return "board/boardwrite";
+    }
+
+    @PostMapping("/board1/writecontroller")
+    @ResponseBody
+    public RedirectView boardwritecontroller(BoardDto boardDto) {
+        HttpSession session = request.getSession();
+        MemberDto memberDto = (MemberDto) session.getAttribute("logindto");
+        boolean result = BoardService.boardwrite(boardDto, memberDto.getMno());
+        if (result) {
+            return new RedirectView("/board/boardlist");
+        } else {
+            return new RedirectView("/board/boardwrite");
+        }
     }
 
     // 게시물 수정
---
+++ src/main/java/com/study/springboardpractice1/service/BoardService.java
@@ -0,0 +1,7 @@
+package com.study.springboardpractice1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+}
